21ebda9509cd32543e8499ecadf8edff5640761e
Joshua Tan <joshuatan@Joshuas-MacBook-Pro-9.local>
2025-04-24 08:40:26 -0400

    Level 2 Complete

---
+++ level_2.py
@@ -0,0 +1,5 @@
+mix_of_stuff = [7,"cymbal",0.75, True]
+
+playlist_for_coding = ["The Hero, The Saint, The Tyrant, & The Terrorist by The Reign of Kindo", "SHIORI by ClariS", "Lydia by F.I.R", "6,8,12 by Brian McKnight", "Tek It by Cafune"]
+
+playlist_for_coding.append("Beyond Mediocrity by 9m88")
